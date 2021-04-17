d66ee4c91cac1c64cd436eeb580eada5f8ff80fe
Andrea Dubuc <76505823+AndreaDubuc@users.noreply.github.com>
2021-04-17 11:24:10 -0400

    finished working on search function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input-box");
   let h1 = document.querySelector("h1");
-  if (searchInput) {
+  if (searchInput.value) {
+    h1.innerHTML = `${searchInput.value}`;
+  } else {
+    alert("Type a city, silly 🤪 ");
   }
-  h1.innerHTML = `${searchInput.value}`;
 }
 
 let searchCity = document.querySelector("#search-form");
