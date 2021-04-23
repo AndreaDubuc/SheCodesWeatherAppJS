432ccea6a756c4fab9f8d5006f3578848e0b6d59
Andrea Dubuc <76505823+AndreaDubuc@users.noreply.github.com>
2021-04-23 08:28:15 -0400

    Changes to JS

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#input-box");
-  let h1 = document.querySelector("h1");
-  if (searchInput.value) {
-    h1.innerHTML = `${searchInput.value}`.toUpperCase().trim();
-  } else {
-    alert("Type a city, silly 🤪 ");
-  }
-}
-
-let searchCity = document.querySelector("#search-form");
-console.log(searchCity);
-searchCity.addEventListener("submit", search);
-
 let now = new Date();
 let hours = now.getHours();
 
@@ -34,27 +19,55 @@ let days = [
 ];
 let day = days[now.getDay()];
 let h2 = document.querySelector("h2");
-h2.innerHTML = `${day.toUpperCase()} </br> ${hours}:${minutes}`;
-
-// Added a function to change the temperature from celsius to fahrenheit.
-let celsiusUnit = document.querySelector("#celsius-link");
-
-function changeUnitC() {
-  let celsiusTemperature = document.querySelector("#temperature");
-  celsiusTemperature.innerHTML = `14 `;
-}
-
-celsiusUnit.addEventListener("click", changeUnitC);
-
-function changeUnitF() {
-  let fahrenheitTemperature = document.querySelector("#temperature");
-  fahrenheitTemperature.innerHTML = `57`;
-}
-
-let fahrenheitUnit = document.querySelector("#fahrenheit-link");
-fahrenheitUnit.addEventListener("click", changeUnitF);
+h2.innerHTML = `${day.toUpperCase()} <br/> ${hours}:${minutes}`;
 
 // API
 
-let apiKey = "775e9c304f4c99854ae283105fb24c72";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=New York";
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "775e9c304f4c99854ae283105fb24c72";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+
+  axios.get(`${apiUrl}`).then(showTemperature);
+}
+
+let currentTemp = document.querySelector("#temperature");
+let city = document.querySelector(".city");
+let day_hour = document.querySelector(".day-hour");
+let weather_description = document.querySelector(".weather-description");
+
+function showTemperature(response) {
+  console.log(response);
+  let temperature = Math.round(response.data.main.temp);
+  currentTemp.innerHTML = `${temperature}`;
+  let userCity = response.data.name;
+  city.innerHTML = `${userCity.toUpperCase().trim()}`;
+  let description = response.data.weather[0].description;
+  console.log(description.toUpperCase());
+  weather_description.innerHTML = `${description.toUpperCase()}`;
+  //FIND OUT HOW TO USE TIMEZONE FROM API TO GET TIME IN USER CITY
+}
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+  city.innerHTML = `...`;
+} else {
+  alert(`Enter a city, silly 🤪`);
+}
+
+// Search City
+
+function search(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#input-box").value;
+  let apiKey = "775e9c304f4c99854ae283105fb24c72";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showTemperature);
+}
+
+let searchCity = document.querySelector("#search-form");
+searchCity.addEventListener("submit", search);
